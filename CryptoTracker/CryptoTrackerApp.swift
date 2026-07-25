563802762023957ecb7fc4f4398e522d01e3147a
MahmoudHassan9 <mahmoudhassanelgrawany33@gmail.com>
2026-07-25 20:38:57 +0300

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Mahmoud hassan on 25/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
