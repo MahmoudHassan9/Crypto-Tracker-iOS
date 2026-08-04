e38c0f720d8d1ee62e1c33617ed0e8b15e354c35
MahmoudHassan9 <mahmoudhassanelgrawany33@gmail.com>
2026-08-05 00:21:57 +0300

    add custom launch screen .

---
+++ CryptoTracker/Core/Launch/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  CryptoTracker
+//
+//  Created by Mahmoud hassan on 04/08/2026.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
